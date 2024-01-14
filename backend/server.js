b41715ab2593e45486360707702d12507d62ae0a
unknown <it22605498@my.sliit.lk>
2024-01-14 13:45:20 +0530

    done all

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.port || 5000;
+const app= express();
+
+app.get('/',(req,res) => res.send('Server is ready!!!'));
+app.listen(port,()=>console.log(`Server started on port ${port}`));
+
