0a6c86fa31a32a9e10ad8470003d416aac87adef
unknown <it22605498@my.sliit.lk>
2024-01-17 00:25:52 +0530

    forntend

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+
+//1.31 min resume
