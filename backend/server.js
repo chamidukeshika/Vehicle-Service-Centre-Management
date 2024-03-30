101badb2a6d1ad792e9c391469c1e63856cf5a8b
unknown <it22605498@my.sliit.lk>
2024-03-30 22:02:21 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 dotenv.config();
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
