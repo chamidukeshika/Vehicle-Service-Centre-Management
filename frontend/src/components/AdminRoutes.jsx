51479298317e71363ef7946c1478b843eb3b00bf
Shaini-Ransara <ransarajayaweera@gmail.com>
2024-04-02 12:55:14 +0530

    test shaini

--- frontend/src/components/AdminRoutes.jsx
+++ frontend/src/components/AdminRoutes.jsx
@@ -4,6 +4,8 @@ import React, { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+console.log(" f");
+
 const AdminRoutes = () => {
     const { userInfo } = useSelector((state) => state.auth);
 
